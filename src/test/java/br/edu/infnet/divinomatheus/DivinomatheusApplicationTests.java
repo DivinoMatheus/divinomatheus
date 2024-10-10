8b6dcea9deafa8bf9d276f1e3c329e999dc99cb4
Divino Matheus <divinomatheusyg@gmail.com>
2024-10-09 21:43:00 -0300

    chore: first project commit

---
+++ src/test/java/br/edu/infnet/divinomatheus/DivinomatheusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.divinomatheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivinomatheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
